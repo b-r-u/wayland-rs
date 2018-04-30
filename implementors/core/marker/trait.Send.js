956af0625f0cef9a922b29662646afab062d7871
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-04-30 19:44:00 +0000

    Deploy Smithay/wayland-rs to github.com/Smithay/wayland-rs.git:gh-pages

---
+++ implementors/core/marker/trait.Send.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["wayland_client"] = ["impl&lt;I:&nbsp;<a class=\"trait\" href=\"wayland_commons/trait.Interface.html\" title=\"trait wayland_commons::Interface\">Interface</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"wayland_client/struct.Proxy.html\" title=\"struct wayland_client::Proxy\">Proxy</a>&lt;I&gt;","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"wayland_client/cursor/struct.CursorTheme.html\" title=\"struct wayland_client::cursor::CursorTheme\">CursorTheme</a>","impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"wayland_client/cursor/struct.Cursor.html\" title=\"struct wayland_client::cursor::Cursor\">Cursor</a>&lt;'a&gt;","impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"wayland_client/cursor/struct.CursorImageBuffer.html\" title=\"struct wayland_client::cursor::CursorImageBuffer\">CursorImageBuffer</a>&lt;'a&gt;","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"wayland_client/egl/struct.WlEglSurface.html\" title=\"struct wayland_client::egl::WlEglSurface\">WlEglSurface</a>",];
+implementors["wayland_server"] = ["impl&lt;I:&nbsp;<a class=\"trait\" href=\"wayland_commons/trait.Interface.html\" title=\"trait wayland_commons::Interface\">Interface</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"wayland_server/struct.Resource.html\" title=\"struct wayland_server::Resource\">Resource</a>&lt;I&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
